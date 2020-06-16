f9a68004c3cbb382762d3bfe18c56377106db790
Injun Song <ijsong@gmail.com>
2020-06-16 14:32:13 +0900

    Remove sequencer and unused codes (#24)
    * add types package
    
    * add type package to define types
    
    * remove unused data types
    
    * remove unused data and logic

--- cmd/metadata_repository/main.go
+++ cmd/metadata_repository/main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"log"
-	"net"
 	"runtime"
-
-	"github.com/kakao/varlog/internal/metadata_repository"
-	pb "github.com/kakao/varlog/proto/metadata_repository"
-	"google.golang.org/grpc"
 )
 
 func main() {
 	log.Printf("GOMAXPROCS: %v\n", runtime.GOMAXPROCS(0))
-	lis, err := net.Listen("tcp", ":9091")
-	if err != nil {
-		log.Fatalf("could not listen: %v", err)
-	}
-	metaRepos := metadata_repository.NewInMemoryMetadataRepository()
-	service := metadata_repository.NewMetadataRepositoryService(metaRepos)
-	s := grpc.NewServer()
-	pb.RegisterMetadataRepositoryServiceServer(s, service)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("could not serve: %v", err)
-	}
+	/*
+		lis, err := net.Listen("tcp", ":9091")
+		if err != nil {
+			log.Fatalf("could not listen: %v", err)
+		}
+		metaRepos := metadata_repository.NewInMemoryMetadataRepository()
+		service := metadata_repository.NewMetadataRepositoryService(metaRepos)
+		s := grpc.NewServer()
+		pb.RegisterMetadataRepositoryServiceServer(s, service)
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("could not serve: %v", err)
+		}
+	*/
 }
--- pkg/varlog/errors.go
+++ pkg/varlog/errors.go
@@ -3,10 +3,8 @@ package varlog
 import "errors"
 
 var (
-	ErrInvalidEpoch      = errors.New("invalid epoch")
 	ErrUnwrittenLogEntry = errors.New("unwritten log entry")
 	ErrWrittenLogEntry   = errors.New("already written log entry")
-	ErrSealedEpoch       = errors.New("sealed epoch")
 	ErrTrimmedLogEntry   = errors.New("already trimmed log entry")
 
 	ErrInvalidProjection = errors.New("invalid projection")
