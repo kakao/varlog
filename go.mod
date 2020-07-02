7f6b84f6f271aee20049c7f7849cc116475c5a66
jun(송인준) <jun.song@kakaocorp.com>
2020-07-02 16:42:24 +0900

    SN: ReplicatorClient (#52)
    * ReplicatorClient
    
    * fix: Makefile
    
    * change: Replicator -> ReplicatorService
    
    * fix: proto
    
    * fix

--- go.mod
+++ go.mod
@@ -6,27 +6,16 @@ require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
-	github.com/cosiner/argv v0.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/go-delve/delve v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/mock v1.4.3
-	github.com/google/uuid v1.1.1 // indirect
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/peterh/liner v1.2.0 // indirect
 	github.com/prometheus/client_golang v1.7.0 // indirect
-	github.com/prometheus/common v0.10.0 // indirect
 	github.com/smartystreets/goconvey v1.6.4
-	github.com/spf13/cobra v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.etcd.io/etcd v0.0.0-20200520232829-54ba9589114f
-	go.starlark.net v0.0.0-20200619143648-50ca820fafb9 // indirect
 	go.uber.org/zap v1.15.0
-	golang.org/x/arch v0.0.0-20200511175325-f7c78586839d // indirect
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	golang.org/x/tools v0.0.0-20191127201027-ecd32218bd7f // indirect
 	google.golang.org/grpc v1.26.0
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ internal/storage/replicator_client.go
@@ -0,0 +1,148 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"sync"
+
+	"github.daumkakao.com/varlog/varlog/pkg/varlog"
+	"github.daumkakao.com/varlog/varlog/pkg/varlog/types"
+	pb "github.daumkakao.com/varlog/varlog/proto/storage_node"
+)
+
+type ReplicatorClient interface {
+	Run(ctx context.Context)
+	Close() error
+	Replicate(ctx context.Context, llsn types.LLSN, data []byte) <-chan error
+}
+
+type replicatorClient struct {
+	rpcConn   *varlog.RpcConn
+	rpcClient pb.ReplicatorServiceClient
+	once      sync.Once
+
+	cancel context.CancelFunc
+
+	mu        sync.RWMutex
+	m         map[types.LLSN]chan<- error
+	stream    pb.ReplicatorService_ReplicateClient
+	requestC  chan *pb.ReplicationRequest
+	responseC chan *pb.ReplicationResponse
+}
+
+func NewReplicatorClient(ctx context.Context, address string) (ReplicatorClient, error) {
+	rpcConn, err := varlog.NewRpcConn(address)
+	if err != nil {
+		return nil, err
+	}
+	return NewReplicatorClientFromRpcConn(ctx, rpcConn)
+}
+
+func NewReplicatorClientFromRpcConn(ctx context.Context, rpcConn *varlog.RpcConn) (ReplicatorClient, error) {
+	rpcClient := pb.NewReplicatorServiceClient(rpcConn.Conn)
+	stream, err := rpcClient.Replicate(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &replicatorClient{
+		rpcConn:   rpcConn,
+		rpcClient: rpcClient,
+		stream:    stream,
+		m:         make(map[types.LLSN]chan<- error),
+		requestC:  make(chan *pb.ReplicationRequest),
+		responseC: make(chan *pb.ReplicationResponse),
+	}, nil
+}
+
+func (rc *replicatorClient) Run(ctx context.Context) {
+	rc.once.Do(func() {
+		ctx, cancel := context.WithCancel(ctx)
+		rc.cancel = cancel
+		go rc.dispatchRequestC(ctx)
+	})
+}
+
+func (rc *replicatorClient) Close() error {
+	rc.cancel()
+	return rc.rpcConn.Close()
+}
+
+func (rc *replicatorClient) Replicate(ctx context.Context, llsn types.LLSN, data []byte) <-chan error {
+	req := &pb.ReplicationRequest{
+		LLSN:    llsn,
+		Payload: data,
+	}
+	errC := make(chan error, 1)
+	rc.mu.Lock()
+	rc.m[llsn] = errC
+	rc.mu.Unlock()
+
+	select {
+	case rc.requestC <- req:
+		return errC
+	case <-ctx.Done():
+	}
+	rc.propagateError(llsn, ctx.Err())
+	return errC
+}
+
+func (rc *replicatorClient) dispatchRequestC(ctx context.Context) {
+	defer rc.stream.CloseSend()
+	for {
+		select {
+		case req := <-rc.requestC:
+			err := rc.stream.Send(req)
+			if err != nil {
+				rc.propagateError(req.GetLLSN(), err)
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (rc *replicatorClient) dispatchResponseC(ctx context.Context) {
+LOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			rsp, err := rc.stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				break LOOP
+			}
+			rc.propagateError(rsp.GetLLSN(), err)
+		}
+	}
+	rc.propagateAllError()
+}
+
+func (rc *replicatorClient) propagateError(llsn types.LLSN, err error) {
+	rc.mu.Lock()
+	errC, ok := rc.m[llsn]
+	if ok {
+		delete(rc.m, llsn)
+	}
+	rc.mu.Unlock()
+	if !ok {
+		panic("no such LLSN")
+	}
+	errC <- err
+	close(errC)
+}
+
+func (rc *replicatorClient) propagateAllError() {
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+	for llsn, errC := range rc.m {
+		delete(rc.m, llsn)
+		errC <- fmt.Errorf("replication channel broken")
+		close(errC)
+	}
+}
