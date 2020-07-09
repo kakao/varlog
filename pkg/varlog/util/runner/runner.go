d24965927226834e5d4682bf84ad63574ff4e650
Injun Song <ijsong@gmail.com>
2020-07-09 10:48:07 +0900

    add mutex to runner (#62)

--- pkg/varlog/util/runner/runner.go
+++ pkg/varlog/util/runner/runner.go
@@ -7,9 +7,12 @@ import (
 
 type Runner struct {
 	wg sync.WaitGroup
+	m  sync.Mutex
 }
 
 func (r *Runner) Run(ctx context.Context, f func(context.Context)) {
+	r.m.Lock()
+	defer r.m.Unlock()
 	r.wg.Add(1)
 	go func() {
 		defer r.wg.Done()
@@ -18,5 +21,7 @@ func (r *Runner) Run(ctx context.Context, f func(context.Context)) {
 }
 
 func (r *Runner) CloseWait() {
+	r.m.Lock()
+	defer r.m.Unlock()
 	r.wg.Wait()
 }
