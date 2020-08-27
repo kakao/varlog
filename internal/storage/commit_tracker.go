e7db539a8a0e0c6cdf06e06e1800efd575e4d070
Injun Song <ijsong@gmail.com>
2020-08-27 15:48:36 +0900

    Rename: commitWaitMap -> commitTrackerMap (#127)

---
+++ internal/storage/commit_tracker.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"sync"
+
+	"github.com/kakao/varlog/pkg/varlog/types"
+)
+
+type commitTrackerMap struct {
+	mu sync.RWMutex
+	m  map[types.LLSN]*appendTask
+}
+
+func newCommitTracker() commitTrackerMap {
+	return commitTrackerMap{m: make(map[types.LLSN]*appendTask)}
+}
+
+func (trk *commitTrackerMap) get(llsn types.LLSN) (*appendTask, bool) {
+	trk.mu.RLock()
+	t, ok := trk.m[llsn]
+	trk.mu.RUnlock()
+	return t, ok
+}
+
+func (trk *commitTrackerMap) put(t *appendTask) {
+	trk.mu.Lock()
+	trk.m[t.llsn] = t
+	trk.mu.Unlock()
+}
+
+func (trk *commitTrackerMap) del(llsn types.LLSN) {
+	trk.mu.Lock()
+	delete(trk.m, llsn)
+	trk.mu.Unlock()
+}
