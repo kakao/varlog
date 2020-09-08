8ac4501a7654df5a0234a26c403679ff6b9b15b9
Injun Song <ijsong@gmail.com>
2020-09-08 11:54:08 +0900

    sn: simplify in memory storage (#144)
    * sn: simplify in memory storage and implement scan api
    
    This change simplifies codes of InMemoryStorage and its unittests.
    Previous implementations had unnecessary variables to maintain committed
    and uncommitted entries. In this change, InMemoryStorage has two slices
    to represent written entries and committed entries, so it doesn't need
    variables to indicate boundaries of committed and uncommitted entries.
    
    Scan API is implemented in this patch. But signature of scan will be
    changed. It will take two arguments:
    
    - offset: starting position of scanned log entries
    - length: length of scanning
    
    Fixes #VARLOG-108
    
    * sn,storage: fix Scanner.Next by code review
    
    See https://github.com/kakao/varlog/pull/144#discussion_r206727

--- internal/storage/in_memory_storage_test.go
+++ internal/storage/in_memory_storage_test.go
@@ -12,54 +12,91 @@ func TestInMemoryStorage(t *testing.T) {
 		s := NewInMemoryStorage()
 
 		Convey("it should not read unwritten data", func() {
-			_, err := s.Read(types.GLSN(10))
+			_, err := s.Read(types.MinGLSN)
 			So(err, ShouldNotBeNil)
 		})
 
-		Convey("it should not write data with wrong LLSN", func() {
-			err := s.Write(types.LLSN(1), nil)
+		Convey("it should not commit unwritten data", func() {
+			err := s.Commit(types.MinLLSN, types.MinGLSN)
 			So(err, ShouldNotBeNil)
 		})
 
 		Convey("it should write data", func() {
-			err := s.Write(types.LLSN(0), []byte("log_001"))
+			err := s.Write(types.MinLLSN, []byte("log_001"))
 			So(err, ShouldBeNil)
 
 			Convey("it should not read uncommitted data", func() {
-				_, err := s.Read(types.GLSN(10))
+				_, err := s.Read(types.MinGLSN)
+				So(err, ShouldNotBeNil)
+			})
+
+			Convey("it should not commit unwritten data", func() {
+				err := s.Commit(types.MinLLSN+1, types.MinGLSN)
 				So(err, ShouldNotBeNil)
 			})
 
 			Convey("it should commit written data", func() {
-				err := s.Commit(types.LLSN(0), types.GLSN(10))
+				err := s.Commit(types.MinLLSN, types.GLSN(2))
 				So(err, ShouldBeNil)
 
 				Convey("it should read committed data", func() {
-					data, err := s.Read(types.GLSN(10))
+					data, err := s.Read(types.GLSN(2))
 					So(err, ShouldBeNil)
 					So(string(data), ShouldEqual, "log_001")
+				})
 
+				Convey("it should not read uncommitted data", func() {
+					_, err := s.Read(types.GLSN(1))
+					So(err, ShouldNotBeNil)
 				})
 
 				Convey("it should delete written data with the same GLSN", func() {
-					num, err := s.Delete(types.GLSN(10))
+					num, err := s.Delete(types.GLSN(2))
 					So(err, ShouldBeNil)
 					So(num, ShouldEqual, 1)
-				})
 
-				Convey("it should delete written data with greater GLSN", func() {
-					num, err := s.Delete(types.GLSN(11))
-					So(err, ShouldBeNil)
-					So(num, ShouldEqual, 1)
-				})
-
-				Convey("it should not delete written data with less GLSN", func() {
-					num, err := s.Delete(types.GLSN(9))
-					So(err, ShouldBeNil)
-					So(num, ShouldEqual, 0)
+					Convey("it should not read deleted data", func() {
+						_, err := s.Read(types.GLSN(2))
+						So(err, ShouldNotBeNil)
+					})
 				})
 			})
 
+			Convey("it should not write data at wrong LLSN", func() {
+				err := s.Write(types.LLSN(3), []byte("log_003"))
+				So(err, ShouldNotBeNil)
+			})
+
+			Convey("it sholud write data at next position", func() {
+				err := s.Write(types.LLSN(2), []byte("log_002"))
+				So(err, ShouldBeNil)
+
+				err = s.Commit(types.LLSN(1), types.GLSN(2))
+				So(err, ShouldBeNil)
+
+				err = s.Commit(types.LLSN(2), types.GLSN(4))
+				So(err, ShouldBeNil)
+
+				Convey("it should scan log entries", func() {
+					scanner, err := s.Scan(types.GLSN(1))
+					So(err, ShouldBeNil)
+
+					ent, err := scanner.Next()
+					So(err, ShouldBeNil)
+					So(ent.LLSN, ShouldEqual, types.LLSN(1))
+					So(ent.GLSN, ShouldEqual, types.GLSN(2))
+					So(ent.Data, ShouldResemble, []byte("log_001"))
+
+					ent, err = scanner.Next()
+					So(err, ShouldBeNil)
+					So(ent.LLSN, ShouldEqual, types.LLSN(2))
+					So(ent.GLSN, ShouldEqual, types.GLSN(4))
+					So(ent.Data, ShouldResemble, []byte("log_002"))
+
+					_, err = scanner.Next()
+					So(err, ShouldNotBeNil)
+				})
+			})
 		})
 	})
 }
