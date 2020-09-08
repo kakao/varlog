8504c51fa59e425950a0ddf66217528e4e74ff05
jun(송인준) <jun.song@kakaocorp.com>
2020-09-08 11:54:08 +0900

    sn: simplify in memory storage (#144)
    * sn: simplify in memory storage and implement scan api
    
    This change simplifies codes of InMemoryStorage and its unittests.
    Previous implementations had unnecessary variables to maintain committed
    and uncommitted entries. In this change, InMemoryStorage has two slices
    to represent written entries and committed entries, so it doesn't need
    variables to indicate boundaries of committed and uncommitted entries.
    
    Scan API is implemented in this patch. But signature of scan will be
    changed. It will take two arguments:
    
    - offset: starting position of scanned log entries
    - length: length of scanning
    
    Fixes #VARLOG-108
    
    * sn,storage: fix Scanner.Next by code review
    
    See https://github.daumkakao.com/varlog/varlog/pull/144#discussion_r206727

--- internal/storage/in_memory_storage.go
+++ internal/storage/in_memory_storage.go
@@ -8,133 +8,191 @@ import (
 	"github.daumkakao.com/varlog/varlog/pkg/varlog/types"
 )
 
-type imLogEntry struct {
-	data      []byte
-	llsn      types.LLSN
-	glsn      types.GLSN
-	committed bool
+type writtenEntry struct {
+	llsn types.LLSN
+	data []byte
 }
 
-type imLogEntrySorter []imLogEntry
-
-func (s imLogEntrySorter) Len() int {
-	return len(s)
-}
-
-func (s imLogEntrySorter) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s imLogEntrySorter) Less(i, j int) bool {
-	return s[i].glsn < s[j].glsn
+type committedEntry struct {
+	llsn types.LLSN
+	glsn types.GLSN
 }
 
 type InMemoryStorage struct {
-	nextLLSN          types.LLSN
-	committedEndIdx   uint64
-	uncommittedEndIdx uint64
-	entries           []imLogEntry
-	m                 sync.RWMutex
+	muWritten sync.RWMutex
+	written   []writtenEntry
+
+	muCommitted sync.RWMutex
+	committed   []committedEntry
+}
+
+type InMemoryScanner struct {
+	cursor  types.LLSN
+	storage *InMemoryStorage
+}
+
+func (s *InMemoryScanner) Next() (varlog.LogEntry, error) {
+	s.storage.muCommitted.RLock()
+	defer s.storage.muCommitted.RUnlock()
+	s.storage.muWritten.RLock()
+	defer s.storage.muWritten.RUnlock()
+
+	if len(s.storage.written) == 0 || len(s.storage.committed) == 0 {
+		return varlog.LogEntry{}, varlog.ErrNoEntry
+	}
+
+	idx := uint64(s.cursor - s.storage.written[0].llsn)
+	if idx >= uint64(len(s.storage.committed)) {
+		return varlog.LogEntry{}, varlog.ErrNoEntry
+	}
+
+	went := s.storage.written[idx]
+	cent := s.storage.committed[idx]
+	if went.llsn != cent.llsn {
+		// TODO (jun): storage is broken
+		return varlog.LogEntry{}, varlog.ErrInternal
+	}
+	ret := varlog.LogEntry{
+		LLSN: cent.llsn,
+		GLSN: cent.glsn,
+		Data: went.data,
+	}
+	s.cursor++
+	return ret, nil
 }
 
 func NewInMemoryStorage() Storage {
 	return &InMemoryStorage{}
 }
 
-func (s *InMemoryStorage) Read(glsn types.GLSN) ([]byte, error) {
-	s.m.RLock()
-	defer s.m.RUnlock()
+func (s *InMemoryStorage) searchCommittedEntry(glsn types.GLSN) (int, committedEntry, error) {
+	i := sort.Search(len(s.committed), func(i int) bool { return s.committed[i].glsn >= glsn })
+	if i >= len(s.committed) {
+		return i, committedEntry{}, varlog.ErrNoEntry
+	}
+	if s.committed[i].glsn == glsn {
+		return i, s.committed[i], nil
+	}
+	return i, s.committed[i], varlog.ErrNoEntry
+}
 
+func (s *InMemoryStorage) Read(glsn types.GLSN) ([]byte, error) {
 	s.assert()
 	defer s.assert()
 
-	committedEntries := s.committedEntries()
-	if len(committedEntries) == 0 {
+	s.muCommitted.RLock()
+	defer s.muCommitted.RUnlock()
+	if len(s.committed) == 0 {
 		return nil, varlog.ErrNoEntry
 	}
 
-	if committedEntries[0].glsn > glsn {
+	first := s.committed[0]
+	last := s.committed[len(s.committed)-1]
+	if first.glsn > glsn || last.glsn < glsn {
 		return nil, varlog.ErrNoEntry
-	}
-	if committedEntries[len(committedEntries)-1].glsn < glsn {
-		return nil, varlog.ErrNoEntry
-	}
-	i := sort.Search(len(committedEntries), func(i int) bool {
-		return committedEntries[i].glsn >= glsn
-	})
-	if i < len(committedEntries) && committedEntries[i].glsn == glsn {
-		return committedEntries[i].data, nil
 	}
 
-	return nil, varlog.ErrNoEntry
+	i, _, err := s.searchCommittedEntry(glsn)
+	if err != nil {
+		return nil, varlog.ErrNoEntry
+	}
+	// NB: The LLSN of the first entry of written and committed should be same.
+	// NB: committedEntry[i] and writtenEntry[i] are the same log entry.
+	s.muWritten.RLock()
+	defer s.muWritten.RUnlock()
+	return s.written[i].data, nil
 }
 
 func (s *InMemoryStorage) Scan(glsn types.GLSN) (Scanner, error) {
 	s.assert()
 	defer s.assert()
-	panic("not yet implemented")
+
+	s.muCommitted.RLock()
+	defer s.muCommitted.RUnlock()
+
+	i, cent, _ := s.searchCommittedEntry(glsn)
+	if i >= len(s.committed) {
+		return nil, varlog.ErrNoEntry
+	}
+	scanner := &InMemoryScanner{
+		cursor:  cent.llsn,
+		storage: s,
+	}
+	return scanner, nil
 }
 
 func (s *InMemoryStorage) Write(llsn types.LLSN, data []byte) error {
-	s.m.Lock()
-	defer s.m.Unlock()
-
 	s.assert()
 	defer s.assert()
 
-	if s.nextLLSN != llsn {
+	s.muWritten.Lock()
+	defer s.muWritten.Unlock()
+
+	if len(s.written) > 0 && s.written[len(s.written)-1].llsn+1 != llsn {
 		return varlog.ErrInvalid
 	}
-	entry := imLogEntry{
-		data: data,
-		llsn: llsn,
-	}
-	s.entries = append(s.entries, entry)
-	s.nextLLSN++
-	s.uncommittedEndIdx++
+
+	s.written = append(s.written, writtenEntry{llsn: llsn, data: data})
 	return nil
 }
 
-func (s *InMemoryStorage) Commit(llsn types.LLSN, glsn types.GLSN) error {
-	s.m.Lock()
-	defer s.m.Unlock()
+func (s *InMemoryStorage) searchWrittenEnry(llsn types.LLSN) (writtenEntry, error) {
+	i := sort.Search(len(s.written), func(i int) bool { return s.written[i].llsn >= llsn })
+	if i < len(s.written) && s.written[i].llsn == llsn {
+		return s.written[i], nil
+	}
+	return writtenEntry{}, varlog.ErrNoEntry
+}
 
+func (s *InMemoryStorage) Commit(llsn types.LLSN, glsn types.GLSN) error {
 	s.assert()
 	defer s.assert()
 
-	entry := &s.entries[s.committedEndIdx]
-	if entry.llsn != llsn {
-		panic("broken storage")
+	s.muWritten.RLock()
+	_, err := s.searchWrittenEnry(llsn)
+	s.muWritten.RUnlock()
+	if err != nil {
+		return err
 	}
 
-	entry.glsn = glsn
-	entry.committed = true
-	s.committedEndIdx++
+	s.muCommitted.Lock()
+	defer s.muCommitted.Unlock()
+
+	if len(s.committed) > 0 {
+		last := s.committed[len(s.committed)-1]
+		if last.llsn+1 != llsn || last.glsn >= glsn {
+			return varlog.ErrInvalid
+		}
+	}
+	s.committed = append(s.committed, committedEntry{llsn: llsn, glsn: glsn})
 	return nil
 }
 
 func (s *InMemoryStorage) Delete(glsn types.GLSN) (uint64, error) {
-	s.m.Lock()
-	defer s.m.Unlock()
-
 	s.assert()
 	defer s.assert()
 
-	if s.committedEndIdx == 0 {
-		return 0, nil
+	s.muCommitted.Lock()
+	defer s.muCommitted.Unlock()
+
+	if len(s.committed) == 0 {
+		return 0, varlog.ErrNoEntry
 	}
-	if s.entries[0].glsn > glsn {
-		return 0, nil
+	first := s.committed[0]
+	last := s.committed[len(s.committed)-1]
+	if first.glsn > glsn || last.glsn < glsn {
+		return 0, varlog.ErrNoEntry
 	}
-	committedEntries := s.committedEntries()
-	i := sort.Search(len(committedEntries), func(i int) bool {
-		return committedEntries[i].glsn > glsn
-	})
-	numTrimmed := uint64(i)
-	copy(s.entries, s.entries[numTrimmed:])
-	s.committedEndIdx -= numTrimmed
-	s.uncommittedEndIdx -= numTrimmed
-	return numTrimmed, nil
+
+	i, _, err := s.searchCommittedEntry(glsn)
+	if err == nil {
+		i++
+	}
+	s.committed = s.committed[i:]
+	s.muWritten.Lock()
+	s.written = s.written[i:]
+	s.muWritten.Unlock()
+	return uint64(i), nil
 }
 
 func (s *InMemoryStorage) Close() error {
@@ -142,21 +200,19 @@ func (s *InMemoryStorage) Close() error {
 }
 
 func (s *InMemoryStorage) assert() {
-	if s.committedEndIdx > s.uncommittedEndIdx {
+	s.muCommitted.Lock()
+	defer s.muCommitted.Unlock()
+
+	s.muWritten.Lock()
+	defer s.muWritten.Unlock()
+
+	if len(s.written) < len(s.committed) {
 		goto out
 	}
-	if s.committedEndIdx > 0 && !s.entries[s.committedEndIdx-1].committed {
+	if len(s.committed) > 0 && s.written[0].llsn != s.committed[0].llsn {
 		goto out
 	}
 	return
 out:
 	panic("bad storage")
-}
-
-func (s *InMemoryStorage) committedEntries() []imLogEntry {
-	return s.entries[:s.committedEndIdx]
-}
-
-func (s *InMemoryStorage) uncommittedEntries() []imLogEntry {
-	return s.entries[s.committedEndIdx:s.uncommittedEndIdx]
 }
