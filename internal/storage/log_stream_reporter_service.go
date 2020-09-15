1b20b8f6c21aa89fe159a2212ecdae36232eac4e
Injun Song <ijsong@gmail.com>
2020-09-15 18:14:41 +0900

    all: remove deprecated fields in log_stream_report proto messages (#169)
    This change removes deprecated fields like KnownNextGLSN,
    PrevKnwonNextGLSN, CommittedGLSNBegin, and CommittedGLSNEnd.
    
    Fixes #VARLOG-205

--- internal/storage/log_stream_reporter_service.go
+++ internal/storage/log_stream_reporter_service.go
@@ -21,39 +21,39 @@ func (s *LogStreamReporterService) Register(server *grpc.Server) {
 }
 
 func (s *LogStreamReporterService) GetReport(ctx context.Context, _ *types.Empty) (*pb.LocalLogStreamDescriptor, error) {
-	reportPb := &pb.LocalLogStreamDescriptor{
+	rsp := &pb.LocalLogStreamDescriptor{
 		StorageNodeID: s.StorageNodeID(),
 	}
-	knownNextGLSN, reports, err := s.LogStreamReporter.GetReport(ctx)
+	knownHighWatermark, reports, err := s.LogStreamReporter.GetReport(ctx)
 	if err != nil {
 		return nil, err
 	}
-	reportPb.Uncommit = make([]*pb.LocalLogStreamDescriptor_LogStreamUncommitReport, len(reports))
-	for i, rpt := range reports {
-		reportPb.Uncommit[i] = &pb.LocalLogStreamDescriptor_LogStreamUncommitReport{
-			LogStreamID:           rpt.LogStreamID,
-			UncommittedLLSNOffset: rpt.UncommittedLLSNOffset,
-			UncommittedLLSNLength: rpt.UncommittedLLSNLength,
+	rsp.Uncommit = make([]*pb.LocalLogStreamDescriptor_LogStreamUncommitReport, len(reports))
+	for i, report := range reports {
+		rsp.Uncommit[i] = &pb.LocalLogStreamDescriptor_LogStreamUncommitReport{
+			LogStreamID:           report.LogStreamID,
+			UncommittedLLSNOffset: report.UncommittedLLSNOffset,
+			UncommittedLLSNLength: report.UncommittedLLSNLength,
 		}
 	}
-	reportPb.NextGLSN = knownNextGLSN
-	return reportPb, nil
+	rsp.HighWatermark = knownHighWatermark
+	return rsp, nil
 }
 
 func (s *LogStreamReporterService) Commit(ctx context.Context, req *pb.GlobalLogStreamDescriptor) (*types.Empty, error) {
 	if len(req.CommitResult) == 0 {
 		return &types.Empty{}, nil
 	}
-	nextGLSN := req.GetNextGLSN()
-	prevNextGLSN := req.GetPrevNextGLSN()
+	hwm := req.GetHighWatermark()
+	prevHWM := req.GetPrevHighWatermark()
 	commitResults := make([]CommittedLogStreamStatus, len(req.CommitResult))
 	for i, cr := range req.CommitResult {
 		commitResults[i].LogStreamID = cr.LogStreamID
-		commitResults[i].HighWatermark = nextGLSN
-		commitResults[i].PrevHighWatermark = prevNextGLSN
+		commitResults[i].HighWatermark = hwm
+		commitResults[i].PrevHighWatermark = prevHWM
 		commitResults[i].CommittedGLSNOffset = cr.CommittedGLSNOffset
 		commitResults[i].CommittedGLSNLength = cr.CommittedGLSNLength
 	}
-	err := s.LogStreamReporter.Commit(ctx, nextGLSN, prevNextGLSN, commitResults)
+	err := s.LogStreamReporter.Commit(ctx, hwm, prevHWM, commitResults)
 	return &types.Empty{}, err
 }
