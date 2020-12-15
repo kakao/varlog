2001286461da5b6b07c9aa6e84e5e18276bfcb29
Injun Song <ijsong@gmail.com>
2020-12-15 23:59:39 +0900

    ci/cd: experimental container for vms (#240)
    This uses multi-stage build for vms container image. It can make smaller
    images.
    
    Resolves #VARLOG-274

---
+++ scripts/grpcui_sn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+grpcui \
+    --plaintext \
+    --import-path $(scriptdir)/../vendor \
+    --import-path $(scriptdir)/../proto \
+    --import-path $GOPATH/src \
+    --proto snpb/management.proto \
+    --proto snpb/log_io.proto \
+    $@
