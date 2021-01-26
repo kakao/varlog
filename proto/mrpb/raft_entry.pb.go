e480b6998593308dcddf77033d0b37a3f811fce2
Hyungkeun Jang <pharrell.jang@kakaocorp.com>
2021-01-26 15:40:16 +0900

    GetMetadata should not return nil (#322)
    Resolves #VARLOG-356

--- proto/mrpb/raft_entry.pb.go
+++ proto/mrpb/raft_entry.pb.go
@@ -5,19 +5,17 @@ package mrpb
 
 import (
 	fmt "fmt"
+	_ "github.com/gogo/protobuf/gogoproto"
+	proto "github.com/gogo/protobuf/proto"
+	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
+	_ "github.com/golang/protobuf/ptypes/timestamp"
+	github_com_kakao_varlog_pkg_types "github.com/kakao/varlog/pkg/types"
+	snpb "github.com/kakao/varlog/proto/snpb"
+	varlogpb "github.com/kakao/varlog/proto/varlogpb"
 	io "io"
 	math "math"
 	math_bits "math/bits"
 	time "time"
-
-	_ "github.com/gogo/protobuf/gogoproto"
-	proto "github.com/gogo/protobuf/proto"
-	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
-
-	github_com_kakao_varlog_pkg_types "github.com/kakao/varlog/pkg/types"
-	snpb "github.com/kakao/varlog/proto/snpb"
-	varlogpb "github.com/kakao/varlog/proto/varlogpb"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
--- proto/varlogpb/metadata.pb.go
+++ proto/varlogpb/metadata.pb.go
@@ -6,17 +6,15 @@ package varlogpb
 import (
 	bytes "bytes"
 	fmt "fmt"
+	_ "github.com/gogo/protobuf/gogoproto"
+	proto "github.com/gogo/protobuf/proto"
+	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
+	_ "github.com/golang/protobuf/ptypes/timestamp"
+	github_com_kakao_varlog_pkg_types "github.com/kakao/varlog/pkg/types"
 	io "io"
 	math "math"
 	math_bits "math/bits"
 	time "time"
-
-	_ "github.com/gogo/protobuf/gogoproto"
-	proto "github.com/gogo/protobuf/proto"
-	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
-
-	github_com_kakao_varlog_pkg_types "github.com/kakao/varlog/pkg/types"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
