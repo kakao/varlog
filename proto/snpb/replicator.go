7f04b5b932986f6c808777dc79bc481a2db2fcee
Injun Song <ijsong@gmail.com>
2021-06-09 14:11:31 +0900

    sn: helper method for SyncPosition (#414)
    Resolves #VARLOG-410

---
+++ proto/snpb/replicator.go
@@ -0,0 +1,15 @@
+package snpb
+
+import "github.com/kakao/varlog/pkg/types"
+
+func InvalidSyncPosition() SyncPosition {
+	return SyncPosition{LLSN: types.InvalidLLSN, GLSN: types.InvalidGLSN}
+}
+
+func (sp SyncPosition) Invalid() bool {
+	return sp.LLSN.Invalid() || sp.GLSN.Invalid()
+}
+
+func (sp SyncPosition) LessThan(other SyncPosition) bool {
+	return sp.LLSN < other.LLSN && sp.GLSN < other.GLSN
+}
