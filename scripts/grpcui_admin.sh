0a2f9b360eb13125ef38bd07920a2700af3d4182
jun.song(송인준)/kakao <jun.song@kakaocorp.com>
2021-12-15 20:25:18 +0900

    admin(vms): remove verbose logs about sync (#547)
    Resolves [#VARLOG-629](https://jira.daumkakao.com/browse/VARLOG-629).

---
+++ scripts/grpcui_admin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+grpcui \
+    --plaintext \
+    --import-path ${scriptdir}/../vendor \
+    --import-path ${scriptdir}/../proto \
+    --import-path $GOPATH/src \
+    --proto vmspb/vms.proto \
+    $@
