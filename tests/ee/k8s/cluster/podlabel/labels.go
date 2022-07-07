06203a9eb0312a1cf1476e4ee67a99da18f9293e
Injun Song <ijsong@gmail.com>
2022-07-08 08:31:29 +0900

    ci: separate k8s manifests into gitops and e2e (#691)
    Resolve: [#VARLOG-696](VARLOG-696)

--- tests/ee/k8s/cluster/podlabel/labels.go
+++ tests/ee/k8s/cluster/podlabel/labels.go
@@ -2,40 +2,40 @@ package podlabel
 
 func DefaultAppSelectorAdmin() map[string]string {
 	return map[string]string{
-		"app.kubernetes.io/name":      "varlog",
-		"app.kubernetes.io/component": "varlogadm",
+		"app.kubernetes.io/name":      "varlogadm",
+		"app.kubernetes.io/component": "admin",
 		"app.kubernetes.io/part-of":   "varlog",
 	}
 }
 
 func DefaultAppSelectorMetaRepos() map[string]string {
 	return map[string]string{
-		"app.kubernetes.io/name":      "varlog",
-		"app.kubernetes.io/component": "varlogmr",
+		"app.kubernetes.io/name":      "varlogmr",
+		"app.kubernetes.io/component": "metadata-repository",
 		"app.kubernetes.io/part-of":   "varlog",
 	}
 }
 
 func DefaultAppSelectorMetaReposClear() map[string]string {
 	return map[string]string{
-		"app.kubernetes.io/name":      "varlog",
-		"app.kubernetes.io/component": "varlogmr-clear",
+		"app.kubernetes.io/name":      "varlogmr-clear",
+		"app.kubernetes.io/component": "metadata-repository",
 		"app.kubernetes.io/part-of":   "varlog",
 	}
 }
 
 func DefaultAppSelectorStorageNode() map[string]string {
 	return map[string]string{
-		"app.kubernetes.io/name":      "varlog",
-		"app.kubernetes.io/component": "varlogsn",
+		"app.kubernetes.io/name":      "varlogsn",
+		"app.kubernetes.io/component": "storage-node",
 		"app.kubernetes.io/part-of":   "varlog",
 	}
 }
 
 func DefaultAppSelectorStorageNodeClear() map[string]string {
 	return map[string]string{
-		"app.kubernetes.io/name":      "varlog",
-		"app.kubernetes.io/component": "varlogsn-clear",
+		"app.kubernetes.io/name":      "varlogsn-clear",
+		"app.kubernetes.io/component": "storage-node",
 		"app.kubernetes.io/part-of":   "varlog",
 	}
 }
