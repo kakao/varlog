2cdb74a83f57a735627c353c8b8764c42f7ed138
Injun Song <ijsong@gmail.com>
2022-09-13 12:28:18 +0900

    fix(storagenode): do not log payload of `/varlog.snpb.Management/AddLogStreamReplca`
    Package `github.com/grpc-ecosystem/go-grpc-middleware/logging/zap` does not work well with
    gogoproto's time.
    
    ```
    panic: message *time.Time is neither a v1 or v2 Message [recovered]
    	panic: message *time.Time is neither a v1 or v2 Message
    ```

--- internal/storagenode/init.go
+++ internal/storagenode/init.go
@@ -16,30 +16,24 @@ func init() {
 		"/varlog.snpb.Management/Unseal",
 		"/varlog.snpb.Management/Sync",
 		"/varlog.snpb.Management/Trim",
-
 		"/varlog.snpb.LogIO/Read",
 		"/varlog.snpb.LogIO/Subscribe",
 		"/varlog.snpb.LogIO/SubscribeTo",
 		"/varlog.snpb.LogIO/TrimDeprecated",
-
 		"/varlog.snpb.Replicator/SyncInit",
 	} {
 		grpcHandlerLogAllowList[method] = true
 	}
 
 	for _, method := range []string{
-		"/varlog.snpb.Management/AddLogStreamReplica",
 		"/varlog.snpb.Management/RemoveLogStream",
 		"/varlog.snpb.Management/Seal",
-		"/varlog.snpb.Management/Unseal",
 		"/varlog.snpb.Management/Sync",
 		"/varlog.snpb.Management/Trim",
-
 		"/varlog.snpb.LogIO/Read",
 		"/varlog.snpb.LogIO/Subscribe",
 		"/varlog.snpb.LogIO/SubscribeTo",
 		"/varlog.snpb.LogIO/TrimDeprecated",
-
 		"/varlog.snpb.Replicator/SyncInit",
 	} {
 		grpcPayloadLogAllowList[method] = true
