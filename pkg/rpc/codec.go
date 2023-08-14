dea2f3ddece46f1048ccf2627663b3c03d86b447
Injun Song <ijsong@gmail.com>
2023-08-14 22:20:25 +0900

    chore(lint): ignore lint for compatibility

--- pkg/rpc/codec.go
+++ pkg/rpc/codec.go
@@ -2,8 +2,8 @@ package rpc
 
 import (
 	gogoproto "github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/proto" //nolint:staticcheck
 	"google.golang.org/grpc/encoding"
-	"google.golang.org/protobuf/proto"
 )
 
 const name = "proto"
@@ -18,9 +18,6 @@ func init() {
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
 	if m, ok := v.(gogoproto.Marshaler); ok {
-		if m, ok := v.(gogoproto.Message); ok {
-			return gogoproto.Marshal(m)
-		}
 		return m.Marshal()
 	}
 	return proto.Marshal(v.(proto.Message))
@@ -28,9 +25,6 @@ func (codec) Marshal(v interface{}) ([]byte, error) {
 
 func (codec) Unmarshal(data []byte, v interface{}) error {
 	if m, ok := v.(gogoproto.Unmarshaler); ok {
-		if m, ok := v.(gogoproto.Message); ok {
-			return gogoproto.Unmarshal(data, m)
-		}
 		return m.Unmarshal(data)
 	}
 	return proto.Unmarshal(data, v.(proto.Message))
