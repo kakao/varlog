8ef188664757e32ee89e12f93039fc8989251969
Injun Song <ijsong@gmail.com>
2024-06-05 11:26:44 +0900

    perf(sn): reuse buffer for ReplicateRequest unmarshaling
    Improve unmarshaling performance by reusing buffers for ReplicateRequest in the
    backup replica.
    
    The protobuf message `github.com/kakao/varlog/proto/snpb.(ReplicateRequest)` has
    two slice fields—LLSN (`[]uint64`) and Data (`[][]byte`). The backup replica
    receives replicated log entries from the primary replica via the gRPC service
    `github.com/kakao/varlog/proto/snpb.(ReplicatorServer).Replicate`, which sends
    `ReplicateRequest` messages.
    
    Upon receiving a `ReplicateRequest`, the backup replica unmarshals the message,
    which involves growing slices for fields such as LLSN and Data. This growth
    causes copy overhead whenever the slice capacities need to expand.
    
    To address this, we introduce a new method, `ResetReuse`, for reusing slices
    instead of resetting them completely. The `ResetReuse` method shrinks the slice
    lengths while preserving their capacities, thus avoiding the overhead of
    reallocating memory.
    
    Example implementation:
    
    ```go
    type Message struct {
        Buffer []byte
        // Other fields
    }
    
    func (m *Message) Reset() {
        *m = Message{}
    }
    
    func (m *Message) ResetReuse() {
        s := m.Buffer[:0]
        *m = Message{}
        m.Buffer = s
    }
    ```
    
    Risks:
    
    This approach has potential downsides. Since the heap space consumed by the
    slices is not reclaimed, the storage node's memory consumption may increase.
    Currently, there is no mechanism to shrink the heap usage.
    
    Additionally, this PR changes the generated code. The protobuf compiler can
    revert it, which is contrary to our intention. To catch this mistake, this PR
    includes a unit test (github.com/kakao/varlog/proto/snpb.TestReplicateRequest)
    to verify that the buffer backing the slices is reused.
    
    Resolves: #795
    
    See also: #806

--- proto/snpb/replicator_test.go
+++ proto/snpb/replicator_test.go
@@ -2,9 +2,178 @@ package snpb
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/kakao/varlog/pkg/types"
 )
+
+func getPointers[S ~[]E, E any](elems S) []uintptr {
+	ptrs := make([]uintptr, len(elems))
+	for i := range elems {
+		p := uintptr(unsafe.Pointer(&elems[i]))
+		ptrs[i] = p
+	}
+	return ptrs
+}
+
+func get2DPointers[S ~[][]E, E any](elems S) [][]uintptr {
+	ptrs := make([][]uintptr, len(elems))
+	for i := range elems {
+		ptrs[i] = getPointers(elems[i])
+	}
+	return ptrs
+}
+
+func TestReplicateRequest(t *testing.T) {
+	smallRequest := ReplicateRequest{
+		LLSN: []types.LLSN{1},
+		Data: [][]byte{[]byte("I")},
+	}
+	smallPayload, err := smallRequest.Marshal()
+	require.NoError(t, err)
+
+	mediumRequest := ReplicateRequest{
+		LLSN: []types.LLSN{11, 12, 13, 14, 15},
+		Data: [][]byte{[]byte("XI"), []byte("XII"), []byte("XIII"), []byte("XIV"), []byte("XV")},
+	}
+	mediumPayload, err := mediumRequest.Marshal()
+	require.NoError(t, err)
+
+	largeRequest := ReplicateRequest{
+		LLSN: []types.LLSN{21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
+		Data: [][]byte{[]byte("XXI"), []byte("XXII"), []byte("XXIII"), []byte("XXIV"), []byte("XXV"), []byte("XXVI"), []byte("XXVII"), []byte("XXVIII"), []byte("XXIX"), []byte("XXX")},
+	}
+	laregePayload, err := largeRequest.Marshal()
+	require.NoError(t, err)
+
+	t.Run("EqualLength", func(t *testing.T) {
+		tcs := []struct {
+			name    string
+			req     ReplicateRequest
+			payload []byte
+		}{
+			{
+				name:    "Small",
+				req:     smallRequest,
+				payload: smallPayload,
+			},
+			{
+				name:    "Medium",
+				req:     mediumRequest,
+				payload: mediumPayload,
+			},
+			{
+				name:    "Large",
+				req:     largeRequest,
+				payload: laregePayload,
+			},
+		}
+
+		for _, tc := range tcs {
+			t.Run(tc.name, func(t *testing.T) {
+				var req ReplicateRequest
+
+				err := req.Unmarshal(tc.payload)
+				require.NoError(t, err)
+				require.Equal(t, tc.req, req)
+				wantLLSNPtrs := getPointers(req.LLSN)
+				wantDataPtrs := get2DPointers(req.Data)
+
+				req.ResetReuse()
+				err = req.Unmarshal(tc.payload)
+				require.NoError(t, err)
+				require.Equal(t, tc.req, req)
+				gotLLSNPtrs := getPointers(req.LLSN)
+				gotDataPtrs := get2DPointers(req.Data)
+
+				require.Equal(t, wantLLSNPtrs, gotLLSNPtrs)
+				require.Equal(t, wantDataPtrs, gotDataPtrs)
+			})
+		}
+	})
+
+	t.Run("GrowingLengthGrowingCapacity", func(t *testing.T) {
+		var req ReplicateRequest
+
+		err := req.Unmarshal(smallPayload)
+		require.NoError(t, err)
+		require.Equal(t, smallRequest, req)
+		smallLLSNPtrs := getPointers(req.LLSN)
+		smallDataPtrs := get2DPointers(req.Data)
+		smallLLSNCap := cap(req.LLSN)
+		smallDataCap := cap(req.Data)
+
+		req.ResetReuse()
+		err = req.Unmarshal(mediumPayload)
+		require.NoError(t, err)
+		require.Equal(t, mediumRequest, req)
+		mediumLLSNPtrs := getPointers(req.LLSN)
+		mediumDataPtrs := get2DPointers(req.Data)
+		mediumLLSNCap := cap(req.LLSN)
+		mediumDataCap := cap(req.Data)
+
+		require.NotEqual(t, smallLLSNPtrs, mediumLLSNPtrs)
+		require.NotEqual(t, smallDataPtrs, mediumDataPtrs)
+		require.Less(t, smallLLSNCap, mediumLLSNCap)
+		require.Less(t, smallDataCap, mediumDataCap)
+
+		req.ResetReuse()
+		err = req.Unmarshal(laregePayload)
+		require.NoError(t, err)
+		require.Equal(t, largeRequest, req)
+		largeLLSNPtrs := getPointers(req.LLSN)
+		largeDataPtrs := get2DPointers(req.Data)
+		largeLLSNCap := cap(req.LLSN)
+		largeDataCap := cap(req.Data)
+
+		require.NotEqual(t, mediumLLSNPtrs, largeLLSNPtrs)
+		require.NotEqual(t, mediumDataPtrs, largeDataPtrs)
+		require.Less(t, mediumLLSNCap, largeLLSNCap)
+		require.Less(t, mediumDataCap, largeDataCap)
+	})
+
+	t.Run("ShrinkingLengthEqualCapacity", func(t *testing.T) {
+		var req ReplicateRequest
+
+		err := req.Unmarshal(laregePayload)
+		require.NoError(t, err)
+		require.Equal(t, largeRequest, req)
+		largeLLSNPtrs := getPointers(req.LLSN)
+		largeDataPtrs := get2DPointers(req.Data)
+		largeLLSNCap := cap(req.LLSN)
+		largeDataCap := cap(req.Data)
+
+		req.ResetReuse()
+		err = req.Unmarshal(mediumPayload)
+		require.NoError(t, err)
+		require.Equal(t, mediumRequest, req)
+		mediumLLSNPtrs := getPointers(req.LLSN)
+		mediumDataPtrs := get2DPointers(req.Data)
+		mediumLLSNCap := cap(req.LLSN)
+		mediumDataCap := cap(req.Data)
+
+		require.NotEqual(t, largeLLSNPtrs, mediumLLSNPtrs)
+		require.NotEqual(t, largeDataPtrs, mediumDataPtrs)
+		require.Equal(t, largeLLSNCap, mediumLLSNCap)
+		require.Equal(t, largeDataCap, mediumDataCap)
+
+		req.ResetReuse()
+		err = req.Unmarshal(smallPayload)
+		require.NoError(t, err)
+		require.Equal(t, smallRequest, req)
+		smallLLSNPtrs := getPointers(req.LLSN)
+		smallDataPtrs := get2DPointers(req.Data)
+		smallLLSNCap := cap(req.LLSN)
+		smallDataCap := cap(req.Data)
+
+		require.NotEqual(t, mediumLLSNPtrs, smallLLSNPtrs)
+		require.NotEqual(t, mediumDataPtrs, smallDataPtrs)
+		require.Equal(t, mediumLLSNCap, smallLLSNCap)
+		require.Equal(t, mediumDataCap, smallDataCap)
+	})
+}
 
 func TestSyncPositionInvalid(t *testing.T) {
 	tcs := []struct {
