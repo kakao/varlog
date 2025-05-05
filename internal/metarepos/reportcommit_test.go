6b8b654c551373f9992cd1c8c14aa92904c98c07
Injun Song <ijsong@gmail.com>
2025-05-05 19:03:49 +0900

    test: fix flaky TestReportCommit
    This change fixes a flaky test issue in `TestReportCommit`. The test
    occasionally failed with an "index out of range" error because the `GetReport`
    request was processed by the mock storage node after the last `CommitBatch` in
    `reportsCommits`.
    This was not an issue with the storage node or metadata repository but a problem
    with the test logic.
    
    Example failure log:
    
    ```txt
    2025-05-02T06:55:51.0691510Z panic: runtime error: index out of range [5] with length 5
    2025-05-02T06:55:51.0691890Z
    2025-05-02T06:55:51.0692238Z goroutine 31718 [running]:
    2025-05-02T06:55:51.0692988Z github.com/kakao/varlog/internal/metarepos_test.TestReportCommit.func2({0x1da8510, 0xc000120bf0})
    ```
    
    The fix ensures that the test logic handles cases where the version index
    exceeds the available range by using the last available report.

--- internal/metarepos/reportcommit_test.go
+++ internal/metarepos/reportcommit_test.go
@@ -272,7 +272,20 @@ func TestReportCommit(t *testing.T) {
 
 				knownVersions.Lock()
 				for lsid, ver := range knownVersions.vers {
-					rsp.UncommitReports = append(rsp.UncommitReports, reportsCommits[lsid][ver].report)
+					var report snpb.LogStreamUncommitReport
+					if rcs := reportsCommits[lsid]; len(rcs) <= int(ver) {
+						lastCommit := rcs[len(rcs)-1].expectedCommit
+						report = snpb.LogStreamUncommitReport{
+							LogStreamID:           lastCommit.LogStreamID,
+							UncommittedLLSNOffset: lastCommit.CommittedLLSNOffset + types.LLSN(lastCommit.CommittedGLSNLength),
+							UncommittedLLSNLength: 0,
+							Version:               ver,
+							HighWatermark:         lastCommit.HighWatermark,
+						}
+					} else {
+						report = rcs[ver].report
+					}
+					rsp.UncommitReports = append(rsp.UncommitReports, report)
 				}
 				knownVersions.Unlock()
 				sort.Slice(rsp.UncommitReports, func(i, j int) bool {
