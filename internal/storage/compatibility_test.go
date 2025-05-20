e8386fb9a04cbd1b524cdf03d7c0b65ab5a32f4a
Injun Song <ijsong@gmail.com>
2025-05-20 15:25:15 +0900

    feat(storage): use latest Pebble SST format (FormatVirtualSSTables)
    This commit updates Pebble to use the latest FormatMajorVersion,
    FormatVirtualSSTables, in preparation for upgrading to Pebble v2.
    
    Upgrading FormatMajorVersion in Pebble is transparent, so we do not need manual
    updates to the storage layer.
    
    Note:
    Downgrading to the previous FormatMajorVersion is not possible. Deploy this
    change carefully to existing running clusters.

---
+++ internal/storage/compatibility_test.go
@@ -0,0 +1,106 @@
+package storage
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/stretchr/testify/require"
+)
+
+// basepath is the root directory of this package.
+var basepath string
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	basepath = filepath.Dir(currentFile)
+}
+
+var update = flag.Bool("update", false, "update test data generated from Pebble v1")
+
+func TestStorage_CompatibilityV1(t *testing.T) {
+	wantList := []struct {
+		key   []byte
+		value []byte
+	}{
+		{key: []byte("key1"), value: []byte("value1")},
+		{key: []byte("key2"), value: []byte("value2")},
+		{key: []byte("key3"), value: []byte("value3")},
+	}
+
+	tcs := []struct {
+		golden string
+		old    pebble.FormatMajorVersion
+		new    pebble.FormatMajorVersion
+	}{
+		{
+			golden: "from_FormatMostCompatible_to_FormatFlushableIngest",
+			old:    pebble.FormatMostCompatible,
+			new:    pebble.FormatFlushableIngest,
+		},
+		{
+			golden: "from_FormatMostCompatible_to_FormatVirtualSSTables",
+			old:    pebble.FormatMostCompatible,
+			new:    pebble.FormatVirtualSSTables,
+		},
+		{
+			golden: "from_FormatFlushableIngest_to_FormatVirtualSSTables",
+			old:    pebble.FormatFlushableIngest,
+			new:    pebble.FormatVirtualSSTables,
+		},
+		{
+			golden: "from_FormatVirtualSSTables_to_FormatVirtualSSTables",
+			old:    pebble.FormatVirtualSSTables,
+			new:    pebble.FormatVirtualSSTables,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.golden, func(t *testing.T) {
+			path := filepath.Join(basepath, "testdata", tc.golden)
+			opts := &pebble.Options{}
+
+			if *update {
+				opts.FormatMajorVersion = tc.old
+
+				db, err := pebble.Open(path, opts)
+				require.NoError(t, err)
+
+				for _, kv := range wantList {
+					err = db.Set(kv.key, kv.value, pebble.Sync)
+					require.NoError(t, err)
+				}
+
+				err = db.Close()
+				require.NoError(t, err)
+
+				return
+			}
+
+			testPath := t.TempDir()
+			err := os.CopyFS(testPath, os.DirFS(path))
+			require.NoError(t, err)
+
+			opts.FormatMajorVersion = tc.new
+			db, err := pebble.Open(testPath, opts)
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				err := db.Close()
+				require.NoError(t, err)
+			})
+
+			for _, want := range wantList {
+				got, closer, err := db.Get(want.key)
+				require.NoError(t, err)
+
+				err = closer.Close()
+				require.NoError(t, err)
+
+				require.Equal(t, want.value, got)
+			}
+		})
+	}
+}
