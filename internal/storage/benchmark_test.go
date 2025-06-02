8a310357f265d188972231b53709f1e5aed6c0dd
Injun Song <ijsong@gmail.com>
2025-06-02 15:28:24 +0900

    refactor(storage): rename DataDB/CommitDB to DataStore/CommitStore
    This refactors the storage package by renaming DataDB and CommitDB to DataStore
    and CommitStore, respectively. There aren't any changes in user-facing features
    such as CLI flags or environment variables.

--- internal/storage/benchmark_test.go
+++ internal/storage/benchmark_test.go
@@ -24,7 +24,7 @@ func BenchmarkStorage_WriteBatch(b *testing.B) {
 			name := fmt.Sprintf("batchLen=%d_dataLen=%d", batchLen, dataLen)
 			b.Run(name, func(b *testing.B) {
 				stg := TestNewStorage(b,
-					WithDataDBOptions(
+					WithDataStoreOptions(
 						WithSync(false), // Use only in mac since sync is too slow in mac os.
 						WithL0CompactionThreshold(2),
 						WithL0StopWritesThreshold(1000),
@@ -65,7 +65,7 @@ func BenchmarkStorage_ScanWithGLSN(b *testing.B) {
 	for _, numLogs := range numLogsList {
 		b.Run(fmt.Sprintf("numLogs=%d", numLogs), func(b *testing.B) {
 			stg := TestNewStorage(b,
-				WithDataDBOptions(
+				WithDataStoreOptions(
 					WithSync(false), // Use only in mac since sync is too slow in mac os.
 					WithL0CompactionThreshold(2),
 					WithL0StopWritesThreshold(1000),
@@ -75,7 +75,7 @@ func BenchmarkStorage_ScanWithGLSN(b *testing.B) {
 					WithMemTableStopWritesThreshold(4),
 					WithMaxConcurrentCompaction(3),
 				),
-				WithCommitDBOptions(
+				WithCommitStoreOptions(
 					WithL0CompactionThreshold(2),
 					WithL0StopWritesThreshold(1000),
 					WithLBaseMaxBytes(64<<20),
